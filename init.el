94785bd03e891541ebba81d88c79853ab80c6468
Hemstreet <jon@hemstreet.io>
2020-04-08 18:58:14 +0000

    org mode sandbox

--- init.el
+++ init.el
@@ -17,3 +17,22 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+;; Org-mode config
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org-bullets which-key use-package try))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
